a552f1939e5b6372f7ef8f972dc6170bf0308e99
jagdishlove <jagdish.funn@gmail.com>
2025-02-24 13:40:17 +0000

    updated

---
+++ client/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+   <div className="home">
+    <h1>Jagdish</h1>
+   </div>
+  );
+}
